c032913d6cf31c3e77cf099ec12a278d25577ed9
Nikita Kochetkov <niki999922@yandex.ru>
2019-02-20 20:21:27 +0300

    hard2

---
+++ jar_bat/arrayset/testHard2.cmd
@@ -0,0 +1,2 @@
+java -p . -cp . -m info.kgeorgiy.java.advanced.arrayset NavigableSet ru.ifmo.rain.kochetkov.arrayset.Arrayset
+:: RMDIR /s/q "__Test__Walk__"
--- jar_bat/walk/test.cmd
+++ jar_bat/walk/test.cmd
@@ -1,1 +1,2 @@
 java -p . -cp . -m info.kgeorgiy.java.advanced.walk Walk ru.ifmo.rain.kochetkov.walk.Walk
+RMDIR /s/q "__Test__Walk__"
