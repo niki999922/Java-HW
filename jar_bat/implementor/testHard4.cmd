9e39b335b660042448db5f426f2bd3a1f6c466bd
Nikita Kochetkov <niki999922@yandex.ru>
2019-03-22 03:11:22 +0300

    update test 4, 5

--- jar_bat/implementor/testHard4.cmd
+++ jar_bat/implementor/testHard4.cmd
@@ -1,1 +1,1 @@
-java -cp . -p "java-advanced-2019/artifacts/;java-advanced-2019/lib/" -m info.kgeorgiy.java.advanced.implementor class  ru.ifmo.rain.kochetkov.implementor.Implementor
+java -cp . -p "java-advanced-2019/artifacts/;java-advanced-2019/lib/" -m info.kgeorgiy.java.advanced.implementor class ru.ifmo.rain.kochetkov.implementor.Implementor
