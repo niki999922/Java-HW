186b835dbc2c68b62cfb1f71bbb7164c95494c60
Nikita Kochetkov <niki999922@yandex.ru>
2019-04-12 17:34:47 +0300

    Create testHard8.cmd

---
+++ jar_bat/mapper/testHard8.cmd
@@ -0,0 +1,4 @@
+SET surname=kochetkov
+SET GIT=java-advanced-2019
+SET pathToClasses=out/production/Java_advanced/.
+java -cp %pathToClasses%;%GIT%/lib/.;%GIT%/artifacts/. -p %pathToClasses%;%GIT%/lib/.;%GIT%/artifacts/. -m info.kgeorgiy.java.advanced.mapper list ru.ifmo.rain.%surname%.mapper.ParallelMapperImpl,ru.ifmo.rain.%surname%.concurrent.IterativeParallelism
