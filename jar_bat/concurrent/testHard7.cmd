7f9d9e6eeb962f9e3302dd2ed698d1373fb0fd5b
Nikita Kochetkov <niki999922@yandex.ru>
2019-04-12 17:31:37 +0300

    Update testHard7.cmd

--- jar_bat/concurrent/testHard7.cmd
+++ jar_bat/concurrent/testHard7.cmd
@@ -1,1 +1,4 @@
-java -cp . -p "java-advanced-2019/artifacts/;java-advanced-2019/lib/" -m info.kgeorgiy.java.advanced.concurrent list ru.ifmo.rain.kochetkov.concurent.IterativeParallelism
+SET surname=kochetkov
+SET GIT=java-advanced-2019
+SET pathToClasses=out/production/Java_advanced/.
+java -cp %pathToClasses%;%GIT%/lib/.;%GIT%/artifacts/. -p %pathToClasses%;%GIT%/lib/.;%GIT%/artifacts/. -m info.kgeorgiy.java.advanced.concurrent list ru.ifmo.rain.%surname%.concurrent.IterativeParallelism
